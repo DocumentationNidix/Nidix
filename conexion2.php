8740efda4610999db3afa50f8bf6c8ee30e3f854
kevinadriantrejochavez <kb.trejo.kt@gmail.com>
2021-08-11 12:16:55 -0600

    version0.0.5

--- conexion2.php
+++
@@ -1,8 +0,0 @@
-<?php
-$conn = new mysqli("localhost","miusuario","mipassword","test");
-	
-	if($conn->connect_errno)
-	{
-		echo "No hay conexión: (" . $conn->connect_errno . ") " . $conn->connect_error;
-	}
-?>

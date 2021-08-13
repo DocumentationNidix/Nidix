e24d413159c29cc201860801c9498c7bba53b1c1
kevinadriantrejochavez <kb.trejo.kt@gmail.com>
2021-08-13 17:37:16 -0600

    version0.0.22

--- insertar.php
+++ insertar.php
@@ -1,16 +1,28 @@
 <?php
 $conexion =  mysql_connect("localhost","miusuario","mipassword","test");
 
-include("insertar.php")
+$titulo = $_POST['titulo'];
+$descripcion = $_POST['descripcion'];
+$fecha = $_POST['fecha'];
 
-$nombre = $_POST['titulo'];
-$nombre = $_POST['descripcion'];
-$nombre = $_POST['fecha'];
+if($_FILES["archivo"]) {
+    $nombre_base = basename($_FILES['archivo']["name"]);
+    $nombre_final = date("m-d-y")."-".date("H-i-s")."-". $nombre_base;
+    $ruta ="archivos/".$nombre_final;
+    $subirarchivo = move_uploaded_file($_FILES['archivo']["proyecto_name"], $ruta);
+    if($subirarchivo){
+        $insertar = "INSERT INTO proyectos(titulo, descripcion, fecha, archivo) VALUES  ´('$titulo','$descripcion', '$fecha', '$ruta' )";
+        $resultado = mysqli_query($conexion, $insertarSQL);
+        if($resultado){
+            echo "<script>alert('se ha enviado su informe'); window.location='tabla1.php'</script>";
+            else {
+                printf("Errormessage: %sn", mysqli_error($conexion));
+            }
+        }
+    }
 
- // Insertar datos
-
-$query= "INSERT INTO proyectos(Titulo,Descripcion, Fecha) VALUES 
-('$Titulo','$Descripcion','$Fecha')";
-
+} else {
+    echo"Error al subir archivo";
+}
 
  ?>
